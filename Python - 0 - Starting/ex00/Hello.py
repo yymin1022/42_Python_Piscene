c5d1f07e3d993338199e129271163a1ce6f913ba
yymin1022 <yymin1022@gmail.com>
2025-12-14 10:26:59 +0000

    Update Python 0 : ex00

---
+++ Python - 0 - Starting/ex00/Hello.py
@@ -0,0 +1,11 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+#your code here
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
