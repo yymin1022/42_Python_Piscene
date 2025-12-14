60951e14ddf0d519f01ed71513bd60b8aa71e330
yymin1022 <yymin1022@gmail.com>
2025-12-14 10:44:34 +0000

    Update Python 0 : ex01

---
+++ Python - 0 - Starting/ex01/format_ft_time.py
@@ -0,0 +1,8 @@
+import datetime
+import time
+
+seconds = time.time()
+print(f"Seconds since January 1, 1970: {seconds:,.4f} or {seconds:.2e} in scientific notation")
+
+today = datetime.date.today()
+print(today.strftime("%b %d %Y"))
