d0706fa42f802bfc9d7f87d8cd6b5b113fb2a287
yymin1022 <yymin1022@gmail.com>
2025-12-17 13:06:31 +0000

    Update Python 0 : ex05

--- Python - 0 - Starting/ex05/building.py
+++ Python - 0 - Starting/ex05/building.py
@@ -1,9 +1,41 @@
 """
 Exercise 05: building.py
-Count characters in a string (upper, lower, punctuation, space, digit).
+Count characters in a string
+(upper, lower, punctuation, space, digit).
 """
 import string
 import sys
+
+
+def analyze_text(text: string):
+    """
+    Count characters in text from parameter
+    (upper, lower, punctuation, space, digit)
+    """
+    upper_count = 0
+    lower_count = 0
+    punctuation_count = 0
+    space_count = 0
+    digit_count = 0
+
+    for char in text:
+        if char.isupper():
+            upper_count += 1
+        elif char.islower():
+            lower_count += 1
+        elif char in string.punctuation:
+            punctuation_count += 1
+        elif char.isspace():
+            space_count += 1
+        elif char.isdigit():
+            digit_count += 1
+
+    print(f"The text contains {len(text)} characters:")
+    print(f"{upper_count} upper letters")
+    print(f"{lower_count} lower letters")
+    print(f"{punctuation_count} punctuation marks")
+    print(f"{space_count} spaces")
+    print(f"{digit_count} digits")
 
 
 def main():
@@ -26,6 +58,8 @@ def main():
                     pass
             except KeyboardInterrupt:
                 pass
+
+        analyze_text(text)
     except AssertionError as err:
         print(f'AssertionError: {err}')
 
