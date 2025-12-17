8f3c9b1dea00591cba37f661b7874e41cbd7c370
yymin1022 <yymin1022@gmail.com>
2025-12-17 12:46:13 +0000

    Update Python 0 : ex04

---
+++ Python - 0 - Starting/ex04/whatis.py
@@ -0,0 +1,21 @@
+import sys
+
+if len(sys.argv) == 1:
+    exit()
+
+try:
+    if len(sys.argv) > 2:
+        raise AssertionError('more than one argument is provided')
+
+    try:
+        num = int(sys.argv[1])
+        
+        if num % 2 == 0:
+            print('I\'m Even.')
+        else:
+            print('I\'m Odd.')
+
+    except ValueError:
+        raise AssertionError('argument is not an integer')
+except AssertionError as err:
+    print(f'AssertionError: {err}')
