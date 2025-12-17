6b957efb312bcbac25a9fb26c9b3f5914bfc6227
yymin1022 <yymin1022@gmail.com>
2025-12-17 13:05:25 +0000

    Update Python 0 : ex05

---
+++ Python - 0 - Starting/ex05/building.py
@@ -0,0 +1,16 @@
+"""
+Exercise 05: building.py
+Count characters in a string (upper, lower, punctuation, space, digit).
+"""
+import sys
+
+
+def main():
+    """
+    Main function of building.py
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
