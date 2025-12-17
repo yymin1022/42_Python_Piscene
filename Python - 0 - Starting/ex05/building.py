4d6f37cae57e227bf39e8e7f92151d49b1ef0e67
yymin1022 <yymin1022@gmail.com>
2025-12-17 13:05:47 +0000

    Update Python 0 : ex05

--- Python - 0 - Starting/ex05/building.py
+++ Python - 0 - Starting/ex05/building.py
@@ -2,6 +2,7 @@
 Exercise 05: building.py
 Count characters in a string (upper, lower, punctuation, space, digit).
 """
+import string
 import sys
 
 
@@ -9,7 +10,24 @@ def main():
     """
     Main function of building.py
     """
-    pass
+    try:
+        args = sys.argv
+        text = None
+
+        if len(args) > 2:
+            raise AssertionError('too many arguments')
+        elif len(args) == 2:
+            text = args[1]
+        else:
+            print("What is the text to count?")
+            try:
+                text = sys.stdin.readline()
+                if not text:
+                    pass
+            except KeyboardInterrupt:
+                pass
+    except AssertionError as err:
+        print(f'AssertionError: {err}')
 
 
 if __name__ == "__main__":
