31fc64f9935f633f2097bc579ead6755f3e10d92
yymin1022 <yymin1022@gmail.com>
2025-12-18 14:40:50 +0000

    Update Python 0 : ex02

--- Python - 0 - Starting/ex02/find_ft_type.py
+++ Python - 0 - Starting/ex02/find_ft_type.py
@@ -4,17 +4,17 @@ def all_thing_is_obj(object: any) -> int:
     msg = None
     match object:
         case list():
-            msg = f"List : {obj_type}"
+            msg = f'List : {obj_type}'
         case tuple():
-            msg = f"Tuple : {obj_type}"
+            msg = f'Tuple : {obj_type}'
         case set():
-            msg = f"Set : {obj_type}"
+            msg = f'Set : {obj_type}'
         case dict():
-            msg = f"Dict : {obj_type}"
+            msg = f'Dict : {obj_type}'
         case str():
-            msg = f"{object} is in the kitchen : {obj_type}"
+            msg = f'{object} is in the kitchen : {obj_type}'
         case _:
-            msg = "Type not found"
+            msg = 'Type not found'
     
     print(msg)
 
