b83b8327d64284375bbcfdf6df25ad4f111b6886
yymin1022 <yymin1022@gmail.com>
2025-12-19 00:07:03 +0000

    Update Python 0 : ex04

--- Python - 0 - Starting/ex04/whatis.py
+++ Python - 0 - Starting/ex04/whatis.py
@@ -5,18 +5,18 @@ if len(sys.argv) == 1:
 
 try:
     if len(sys.argv) > 2:
-        raise AssertionError('more than one argument is provided')
+        raise AssertionError("more than one argument is provided")
 
     try:
         num = int(sys.argv[1])
         
         if num % 2 == 0:
-            print('I\'m Even.')
+            print("I\'m Even.")
         else:
-            print('I\'m Odd.')
+            print("I\'m Odd.")
 
     except ValueError:
-        raise AssertionError('argument is not an integer')
+        raise AssertionError("argument is not an integer")
 except AssertionError as err:
-    print(f'AssertionError: {err}')
+    print(f"AssertionError: {err}")
     sys.exit(1)
