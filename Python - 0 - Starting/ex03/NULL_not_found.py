cbeb5140c69a8467d1b5f9954e17f6e96472103c
yymin1022 <yymin1022@gmail.com>
2025-12-19 00:06:59 +0000

    Update Python 0 : ex03

--- Python - 0 - Starting/ex03/NULL_not_found.py
+++ Python - 0 - Starting/ex03/NULL_not_found.py
@@ -3,17 +3,17 @@ def NULL_not_found(object: any) -> int:
 
     match object:
         case None:
-            print(f'Nothing: {object} {obj_type}')
+            print(f"Nothing: {object} {obj_type}")
         case bool() if object is False:
-            print(f'Fake: {object} {obj_type}')
+            print(f"Fake: {object} {obj_type}")
         case float() if object != object:
-            print(f'Cheese: {object} {obj_type}')
+            print(f"Cheese: {object} {obj_type}")
         case int() if object == 0:
-            print(f'Zero: {object} {obj_type}')
+            print(f"Zero: {object} {obj_type}")
         case str() if not object:
-            print(f'Empty: {obj_type}')
+            print(f"Empty: {obj_type}")
         case _:
-            print('Type not found')
+            print("Type not found")
             return 1
 
     return 0
